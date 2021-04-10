b73159c29c6977407842ca1d9ff0af5f7b58a54d
sasuk <sasukeamjed@gmail.com>
2021-04-10 11:17:54 +0400

    adding firebase

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,6 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
         title: Text("riverpod with firebase"),
       ),
       body: Center(
